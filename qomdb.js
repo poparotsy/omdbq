693b185a611cda0d55be368fad503f8743c84dd3
Poparotsy <poparotsy@yahoo.com>
2019-07-03 00:44:30 -0400

    ignore -- format

--- qomdb.js
+++ qomdb.js
@@ -1,15 +1,20 @@
-const https = require('https');
-const url = process.env.omdbUrl || 'https://www.omdbapi.com/?i=tt3896198&apikey=7eff34ec&t=' ;
-const movie = process.env.movie ;
-
+const https = require("https");
+const url =
+  process.env.omdbUrl ||
+  "https://www.omdbapi.com/?i=tt3896198&apikey=7eff34ec&t=";
+const movie = process.env.movie;
 
 var query = url + movie.replace(" ", "+");
 //console.log(process.env.omdbUrl);
 //console.log(process.env.movie);
 //console.log(query);
 
-https.get( query, res => {
-let body = '';
-res.on('data', data => { body += data; });
-res.on('end', () => console.log(JSON.parse(body)));
-}).on('error', error => console.error(error.message));
+https
+  .get(query, res => {
+    let body = "";
+    res.on("data", data => {
+      body += data;
+    });
+    res.on("end", () => console.log(JSON.parse(body)));
+  })
+  .on("error", error => console.error(error.message));
