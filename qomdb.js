3a3260e2ddd0a9b42c32ba072c816c7250be516c
Poparotsy <poparotsy@yahoo.com>
2019-07-03 00:57:31 -0400

    Format ++

--- qomdb.js
+++ qomdb.js
@@ -1,10 +1,9 @@
 const https = require("https");
-const url =
-  process.env.omdbUrl ||
-  "https://www.omdbapi.com/?i=tt3896198&apikey=7eff34ec&t=";
-const movie = process.env.movie;
+const url = process.env.omdbUrl || "https://www.omdbapi.com/?i=tt3896198&apikey=7eff34ec&t=";
+const movie = process.env.movie || "";
 
 var query = url + movie.replace(" ", "+");
+
 //console.log(process.env.omdbUrl);
 //console.log(process.env.movie);
 //console.log(query);
